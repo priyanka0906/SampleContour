10f7592b7917f9cfc3666f8bc114978ca8ff5f34
priyanka0906 <priyanka02amb@reddiffmail.com>
2021-05-23 18:17:27 +0530

    views created

--- app/src/main/java/com/example/samplecontour/MainView.kt
+++ app/src/main/java/com/example/samplecontour/MainView.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.graphics.Color.RED
 import android.graphics.Typeface
 import android.util.AttributeSet
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toolbar
 import com.squareup.contour.ContourLayout
@@ -31,6 +32,17 @@ class MainView(context:Context,attrs: AttributeSet? = null):ContourLayout(contex
         typeface = Typeface.create("sans-serif-medium", Typeface.NORMAL)
     }
 
+    private var confirmPassword = EditText(context).apply{
+        hint = "Confirm Password"
+
+        textSize = 18f
+        typeface = Typeface.create("sans-serif-medium", Typeface.NORMAL)
+    }
+    private val submitBtn = Button(context).apply {
+        text = "Submit"
+      setBackgroundColor(RED)
+
+    }
     init{
 
         toolbar.layoutBy(
@@ -51,6 +63,15 @@ class MainView(context:Context,attrs: AttributeSet? = null):ContourLayout(contex
                 x = leftTo { parent.left()+50.xdip }.rightTo { parent.right()-10.xdip },
                 y = bottomTo{email.bottom()+60.ydip}
 
+        )
+        confirmPassword.layoutBy(
+                x = leftTo { parent.left()+50.xdip }.rightTo { parent.right()-10.xdip },
+                y = bottomTo{password.bottom()+60.ydip}
+
+        )
+        submitBtn.layoutBy(
+                x = leftTo { parent.left()+10.xdip }.rightTo { parent.right()-100.xdip },
+                y = bottomTo{confirmPassword.bottom()+80.ydip}
         )
     }
 }
